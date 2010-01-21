87d3e590b10700379040818da4ebaf44683840be
Daniel Aquino <mr.danielaquino@gmail.com>
2010-01-21 00:49:25 +0000

    fixed the quat class

--- lib/quat.rb
+++ lib/quat.rb
@@ -8,46 +8,32 @@ class Quat < Vector
 	def length
 		Math.sqrt self.dot(self)
 	end
-	def dot q2
-		@x * q2.x + @y * q2.y + @z * q2.z + @w * q2.w
+	def dot q
+		@x * q.x + @y * q.y + @z * q.z + @w * q.w
 	end
 	def normalize
 		d = length
-		q = self.dup
-		q.x /= d
-		q.y /= d
-		q.z /= d
-		q.w /= d
-		q
+		Quat.new( @x / d, @y / d, @z / d, @w / d )
 	end
 	def conjugate
-		q = self.dup
-		q.x = -q.x
-		q.y = -q.y
-		q.z = -q.z
-		q
+		Quat.new( -@x, -@y, -@z )
 	end
-	def cross q2
+	def cross q
 		Quat.new(
-			@y * q2.z - @z * q2.y, # x
-			@z * q2.x - @x * q2.z, # y
-			@x * q2.y - @y * q2.x, # z
-			0
+			@y * q.z - @z * q.y,
+			@z * q.x - @x * q.z,
+			@x * q.y - @y * q.x,
 		)
 	end
-	def * q2
-		q = self.dup
-		q.x = q.w*q2.x + q.x*q2.w + q.y*q2.z - q.z*q2.y
-		q.y = q.w*q2.y - q.x*q2.z + q.y*q2.w + q.z*q2.x
-		q.z = q.w*q2.z + q.x*q2.y - q.y*q2.x + q.z*q2.w
-		q.w = q.w*q2.w - q.x*q2.x - q.y*q2.y - q.z*q2.z
-		q
+	def * q
+		Quat.new(
+			@w * q.x + @x * q.w + @y * q.z - @z * q.y,
+			@w * q.y + @y * q.w + @z * q.x - @x * q.z,
+			@w * q.z + @z * q.w + @x * q.y - @y * q.x,
+			@w * q.w - @x * q.x - @y * q.y - @z * q.z
+		)
 	end
-	def - q2
-		q = self.dup
-		q.x -= q2.x
-		q.y -= q2.y
-		q.z -= q2.z
-		q
+	def - q
+		Quat.new( @x - q.x, @y - q.y, @z - q.z )
 	end
 end
