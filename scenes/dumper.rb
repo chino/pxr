1f85a19cc0f4c0da36cad4d43b933413f2794abd
Daniel Aquino <mr.danielaquino@gmail.com>
2010-02-07 12:43:20 -0500

    cleaned up dumper with whats needed

--- scenes/dumper.rb
+++ scenes/dumper.rb
@@ -4,16 +4,18 @@ $window  = Window.new("Model Viewer", $options[:width], $options[:height], $opti
 # generate js array of colors and vertices
 $colors = []
 $verts = []
-$count = 0
 $model = Model.new("ship.mxv").model
 $model.primitives.each do |primitive|
 	primitive[:verts].each do |index|
-    $count += 1
 		$verts << $model.verts[index][:vector].join(',')
 		$colors << $model.verts[index][:rgba].join(',')
 	end
 end
-puts "var colors = [" + $colors.join(',') + "];"
-puts "var vertices = [" + $verts.join(',') + "];"
-puts "triangleVertexPositionBuffer.numItems = #{$count};"
-puts "triangleVertexColorBuffer.numItems = #{$count};"
+
+puts "var colors = [" 
+puts $colors.join(",\n")
+puts "];"
+
+puts "var vertices = ["
+puts $verts.join(",\n")
+puts "];"
