fd5c25fcaaa1dde5bd0d95bf4c1bbcd8cb10c9bf
Daniel Aquino <mr.danielaquino@gmail.com>
2010-02-07 12:41:35 -0500

    fixed counter for dumper

--- scenes/dumper.rb
+++ scenes/dumper.rb
@@ -4,14 +4,16 @@ $window  = Window.new("Model Viewer", $options[:width], $options[:height], $opti
 # generate js array of colors and vertices
 $colors = []
 $verts = []
+$count = 0
 $model = Model.new("ship.mxv").model
 $model.primitives.each do |primitive|
 	primitive[:verts].each do |index|
+    $count += 1
 		$verts << $model.verts[index][:vector].join(',')
 		$colors << $model.verts[index][:rgba].join(',')
 	end
 end
 puts "var colors = [" + $colors.join(',') + "];"
 puts "var vertices = [" + $verts.join(',') + "];"
-puts "triangleVertexPositionBuffer.numItems = " + ($verts.length*3).to_s + ";"
-puts "triangleVertexColorBuffer.numItems = " + ($colors.length*4).to_s + ";"
+puts "triangleVertexPositionBuffer.numItems = #{$count};"
+puts "triangleVertexColorBuffer.numItems = #{$count};"
