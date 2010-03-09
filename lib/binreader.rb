cc45bb8a6d8acde0ddfce97b2f6f46cced2a0867
Daniel Aquino <mr.danielaquino@gmail.com>
2010-03-08 19:08:54 -0500

    added read_ushort; fixed read_short to read correct sign; fixed read_int to read 4 byte int

--- lib/binreader.rb
+++ lib/binreader.rb
@@ -17,10 +17,13 @@ module BinReader
 		read(1).unpack("C")[0]
 	end
 	def read_int
-		read(4).unpack("v")[0]
+		read(4).unpack("i")[0]
+	end
+	def read_ushort
+		read(2).unpack("S")[0]
 	end
 	def read_short
-		read(2).unpack("S")[0]
+		read(2).unpack("s")[0]
 	end
 	def read_float
 		read(4).unpack("e")[0]
