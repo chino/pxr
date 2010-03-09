8ea2fb83e136433c8e4874babfc5314e13401608
Daniel Aquino <mr.danielaquino@gmail.com>
2010-03-08 19:10:26 -0500

    added tcords to the dumper

--- scenes/dumper.rb
+++ scenes/dumper.rb
@@ -1,17 +1,25 @@
 # so things don't break
 $window  = Window.new("Model Viewer", $options[:width], $options[:height], $options[:fullscreen])
 
+# data
+$model = Model.new("ship.mxv").model
+
 # generate js array of colors and vertices
 $colors = []
 $verts = []
-$model = Model.new("ship.mxv").model
+$tcords = []
 $model.primitives.each do |primitive|
 	primitive[:verts].each do |index|
 		$verts << $model.verts[index][:vector].join(',')
+		$tcords << [
+			$model.verts[index][:tu],
+			$model.verts[index][:tv],
+		].join(',')
 		$colors << $model.verts[index][:rgba].join(',')
 	end
 end
 
+=begin
 puts "var colors = [" 
 puts $colors.join(",\n")
 puts "];"
@@ -19,3 +27,13 @@ puts "];"
 puts "var vertices = ["
 puts $verts.join(",\n")
 puts "];"
+
+puts "var textures = ["
+puts $model.textures.map{|x|"'#{x}'"}.join(",\n")
+puts "];"
+=end
+
+puts "var tcords = ["
+puts $tcords.join(",\n")
+puts "];"
+
