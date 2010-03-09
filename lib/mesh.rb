2b8259e3384fbfea44d568c080495bec1cbd205e
Daniel Aquino <mr.danielaquino@gmail.com>
2010-03-08 19:15:14 -0500

    temporary commit so i can share with lion; sorts out transparent primitives into an array to draw after solids; sets forsaken transparency render states; temporarly disables drawing of solid objects to isolate problem with transparent basement window;  temporarly dumps list of primitives and verts while building display list

--- lib/mesh.rb
+++ lib/mesh.rb
@@ -15,25 +15,43 @@ module Mesh
 			GL.CallList(@dl)
 			return
 		end
+		trans=[]
 		@primitives.each do |primitive|
-			image = Image.get primitive[:texture]
-			if !image.nil? and image.colorkey
-				GL.Enable(GL::ALPHA_TEST)
-				GL.AlphaFunc(GL::GREATER,(100.0/255.0))
+			if primitive[:transparencies]
+				trans << primitive
+				next
 			end
-			image.bind if image
-			GL.PointSize(@size) if @render_type == GL::POINTS and @size
-			GL.Begin @render_type
-			GL.Normal3fv primitive[:normal] if primitive[:normal]
-			primitive[:verts].each do |index|
-				vert = @verts[index]
-				GL.Color4ubv vert[:rgba] if vert[:rgba]
-				GL.TexCoord2f vert[:tu], vert[:tv] if vert[:tu] and vert[:tv]
-				GL.Vertex3fv vert[:vector]
-			end
-			GL.End
-			GL.Disable(GL::ALPHA_TEST) if !image.nil? and image.colorkey
-			image.unbind if image
+#			draw_primitive primitive
 		end
+		GL.DepthMask(GL::FALSE)
+		GL.Enable(GL::BLEND)
+		GL.BlendFunc(GL::SRC_ALPHA,GL::ONE)
+		trans.each_with_index do |primitive,i|
+puts "primitive: #{i}"
+			draw_primitive primitive
+		end
+		GL.DepthMask(GL::TRUE)
+		GL.Disable(GL::BLEND)
+	end
+	def draw_primitive primitive
+		image = Image.get primitive[:texture]
+		if !image.nil? and image.colorkey
+			GL.Enable(GL::ALPHA_TEST)
+			GL.AlphaFunc(GL::GREATER,(100.0/255.0))
+		end
+		image.bind if image
+		GL.PointSize(@size) if @render_type == GL::POINTS and @size
+		GL.Begin @render_type
+		GL.Normal3fv primitive[:normal] if primitive[:normal]
+		primitive[:verts].each_with_index do |index,i|
+			vert = @verts[index]
+			GL.Color4ubv vert[:rgba] if vert[:rgba]
+			GL.TexCoord2f vert[:tu], vert[:tv] if vert[:tu] and vert[:tv]
+			GL.Vertex3fv vert[:vector].map{|x|x.to_i}
+puts "\tvert: #{i} = #{vert[:vector].join(', ')}"
+		end
+		GL.End
+		GL.Disable(GL::ALPHA_TEST) if !image.nil? and image.colorkey
+		image.unbind if image
 	end
 end
