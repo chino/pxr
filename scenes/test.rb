0701f0c6c7f8cc13a4ab1189a8d838012931d67a
Daniel Aquino <mr.danielaquino@gmail.com>
2010-03-08 19:11:58 -0500

    added test scene to fix transparency issue in basement

---
+++ scenes/test.rb
@@ -0,0 +1,66 @@
+$window  = Window.new("Model Viewer", $options[:width], $options[:height], $options[:fullscreen])
+
+GL.Disable(GL::CULL_FACE)
+
+$objects = []
+$level   = Model.new("ship.mxv")
+$objects << $level
+
+$camera     = View.new
+$camera.pos = Vector.new -100,-50,-500
+
+$step = 10
+$movement = Vector.new 0,0,0
+$bindings = {
+	:w => :forward,
+	:s => :back,
+	:e => :up,
+	:d => :down,
+	:f => :left,
+	:g => :right
+}
+$window.keyboard = Proc.new{|key,x,y,pressed|
+	k = key.chr.downcase.to_sym
+	b = $bindings[k]
+	if b.nil?
+		puts "Unknown key binding #{k}"
+		next
+	end
+	#puts "key #{k} #{pressed ? 'pressed':'released'}, binded to #{b}"
+	case b
+	when :right then pressed ? $movement.x += $step : $movement.x = 0 
+	when :left then pressed ? $movement.x -= $step : $movement.x = 0 
+	when :up then pressed ? $movement.y += $step : $movement.y = 0 
+	when :down then pressed ? $movement.y -= $step : $movement.y = 0 
+	when :forward then pressed ? $movement.z += $step : $movement.z = 0 
+	when :back then pressed ? $movement.z -= $step : $movement.z = 0
+	else puts "unknown key binding #{k}"
+	end
+}
+$window.display = Proc.new{
+
+	# read mouse for rotation
+	x,y = Mouse.get
+
+	# apply rotation
+	$camera.rotate x, y
+
+	# apply movement
+	$camera.move $movement
+
+	# modify coordinate system based on camera position
+	$camera.place_camera
+
+	# draw at their locations
+	$objects.each do |o|
+		GL.PushMatrix
+		o.load_matrix
+		o.draw
+		GL.PopMatrix
+	end
+
+	# move back to the camera
+	GL.LoadIdentity
+
+}
+$window.run
