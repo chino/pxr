cd0acd30626013d95b2056bc9c24b160cebba115
Daniel Aquino <mr.danielaquino@gmail.com>
2010-03-22 17:33:49 -0400

    updated to use a texture

--- lib/quad.rb
+++ lib/quad.rb
@@ -4,11 +4,13 @@ class Quad < View
 		super *args
 		@render_type = GL::QUADS
 #		@render_type = GL::POLYGON
-		@scale = Vector.new 100,100,100
+		@scale = Vector.new 1000,1000,1000
+#		@texture = 'data/images/water.jpg'
+		@texture = 'data/images/yelo.png'
 		@verts = []
 		@primitives = []
-		rows=20
-		cols=20
+		rows=10
+		cols=10
 		cols.times do |x|
 			rows.times do |y|
 				add x,y
@@ -17,12 +19,12 @@ class Quad < View
 	end
 	def add x,y
 		l = @verts.length
-		@primitives << { :verts => [ l+0,l+1,l+2,l+3 ] }
+		@primitives << { :verts => [ l+0,l+1,l+2,l+3 ], :texture => @texture }
 #		@primitives << { :verts => [ l+0,l+1,l+2 ] }
-		@verts << { :vector => [x+0,y+0,0] }
-		@verts << { :vector => [x+0,y+1,0] }
-		@verts << { :vector => [x+1,y+1,0] }
-		@verts << { :vector => [x+1,y+0,0] }
+		@verts << { :vector => [x+0,y+0,0], :tu => 0, :tv => 0 }
+		@verts << { :vector => [x+0,y+1,0], :tu => 1, :tv => 0 }
+		@verts << { :vector => [x+1,y+1,0], :tu => 1, :tv => 1 }
+		@verts << { :vector => [x+1,y+0,0], :tu => 0, :tv => 1 }
 	end
 	def make_dl
 	end
