fb79af639771ccff4b70efed4d4e50de15d0abd6
Daniel Aquino <mr.danielaquino@gmail.com>
2010-04-02 17:09:21 -0400

    gave quad a normal; added routines to tell you which side a point lies {front/back/coincide}; added routine to tell you if a point is within the quad

--- lib/quad.rb
+++ lib/quad.rb
@@ -1,5 +1,6 @@
 class Quad < View
 	include Mesh
+	attr_accessor :normal
 	def initialize *args
 		super *args
 		@render_type = GL::QUADS
@@ -9,12 +10,47 @@ class Quad < View
 		@texture = 'data/images/yelo.png'
 		@verts = []
 		@primitives = []
-		rows=100
-		cols=100
+		rows=10
+		cols=10
 		cols.times do |x|
 			rows.times do |y|
 				add x,y
 			end
+		end
+		@normal = Vector.new(0,0,1)
+		max_x = cols * @scale.x
+		max_y = rows * @scale.y
+		@pairs = [
+			[Vector.new(0,0,0),Vector.new(max_x,0,0)],
+			[Vector.new(max_x,0,0),Vector.new(max_x,max_y,0)],
+			[Vector.new(max_x,max_y,0),Vector.new(0,max_y,0)],
+			[Vector.new(0,max_y,0),Vector.new(0,0,0)]
+		]
+	end
+	def within? point
+		@pi2 ||= Math::PI * 2
+		radians = 0
+		@pairs.each do |v1,v2|
+			# vector from point to corner of polygon
+			v1 = (v1 - point).normalize
+			v2 = (v2 - point).normalize
+			# angle between vectors
+			radians += Math.acos( v1.dot(v2) )
+		end
+		puts "#{radians}"
+		radians == @pi2
+	end
+	def d
+		@d ||= (-@normal.x*@pos.x) - (@normal.y*@pos.y) - (@normal.z*@pos.z)
+	end
+	def side point
+		distance = @normal.dot(point) + d
+		if distance > 0.0
+			:front
+		elsif distance < 0.0
+			:back
+		else
+			:coincide
 		end
 	end
 	def add x,y
