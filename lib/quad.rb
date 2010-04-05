e70254f338633924c2771fd808e29507606e1775
Daniel Aquino <mr.danielaquino@gmail.com>
2010-04-05 11:09:19 -0400

    i cant use opengl scaling cause then physics doesnt know about it; i need a way to scale the object manually on creation; or i need a way for physics to automatically compute the scaling

--- lib/quad.rb
+++ lib/quad.rb
@@ -1,53 +1,36 @@
 class Quad < View
 	include Mesh
-	attr_accessor :normal
+	attr_accessor :normal, :pairs, :collision
 	def initialize *args
 		super *args
+		@collision = :mesh
 		@render_type = GL::QUADS
 #		@render_type = GL::POLYGON
-		@scale = Vector.new 1000,1000,1000
-#		@texture = 'data/images/water.jpg'
 		@texture = 'data/images/yelo.png'
+		@normal = [0,0,1]
 		@verts = []
 		@primitives = []
 		rows=10
 		cols=10
 		cols.times do |x|
 			rows.times do |y|
-				add x,y
+				add x, y
 			end
 		end
-		@normal = Vector.new(0,0,1)
-		max_x = cols * @scale.x
-		max_y = rows * @scale.y
-		@pairs = [
-			[Vector.new(0,0,0),Vector.new(max_x,0,0)],
-			[Vector.new(max_x,0,0),Vector.new(max_x,max_y,0)],
-			[Vector.new(max_x,max_y,0),Vector.new(0,max_y,0)],
-			[Vector.new(0,max_y,0),Vector.new(0,0,0)]
-		]
-	end
-	def within? point
-		@pi2 ||= sprintf('%.14f',Math::PI * 2)
-		radians = 0
-		@pairs.each do |v1,v2|
-			# vector from point to corner of polygon
-			v1 = (v1 - point).normalize
-			v2 = (v2 - point).normalize
-			# angle between vectors
-			radians += Math.acos( v1.dot(v2) )
-		end
-		radians = sprintf('%.14f',radians)
-		radians == @pi2
 	end
 	def add x,y
 		l = @verts.length
-		@primitives << { :verts => [ l+0,l+1,l+2,l+3 ], :texture => @texture }
-#		@primitives << { :verts => [ l+0,l+1,l+2 ] }
-		@verts << { :vector => [x+0,y+0,0], :tu => 0, :tv => 0 }
-		@verts << { :vector => [x+0,y+1,0], :tu => 1, :tv => 0 }
-		@verts << { :vector => [x+1,y+1,0], :tu => 1, :tv => 1 }
-		@verts << { :vector => [x+1,y+0,0], :tu => 0, :tv => 1 }
+		@primitives << { 
+			:verts => [ l+0,l+1,l+2,l+3 ],
+#			:verts => [ l+0,l+1,l+2 ],
+			:pos => @pos, # one of poly verts for plane position
+			:normal => @normal,
+			:texture => @texture,
+		}
+		@verts << { :vector => [x,y,0], :tu => 0, :tv => 0 }
+		@verts << { :vector => [x,y,0], :tu => 1, :tv => 0 }
+		@verts << { :vector => [x,y,0], :tu => 1, :tv => 1 }
+		@verts << { :vector => [x,y,0], :tu => 0, :tv => 1 }
 	end
 	def make_dl
 	end
