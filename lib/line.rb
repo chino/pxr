9abe8026825ef751296ee8185264e2dd14cebe4e
Daniel Aquino <mr.danielaquino@gmail.com>
2010-05-23 15:06:02 -0400

    generic line class to draw single/muliple lines

---
+++ lib/line.rb
@@ -0,0 +1,21 @@
+require "model"
+class Line < Model
+	include Mesh
+	def initialize lines=[]
+		@render_type = GL::LINES
+		@verts = []
+		@primitives = []
+		lines.each do |start,stop,color,texture|
+			color ||= [255,255,255,255]
+			@verts << { :vector => start, :rgba => color }
+			@verts << { :vector => stop, :rgba => color }
+			@primitives << {
+				:texture => nil, 
+				:verts => [
+					@verts.length-2,
+					@verts.length-1
+				]
+			}
+		end
+	end
+end
