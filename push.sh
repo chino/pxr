4e9d6e0f53b6f89ff133332249c5f77d8490a7ed
Daniel Aquino <mr.danielaquino@gmail.com>
2010-05-25 03:25:50 -0400

    getting around dns lags on my pc

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
-bzr push bzr+ssh://fly.thruhere.net:2222/projects/pxr/
-ssh fly.thruhere.net -l daquino -p 2222 <<< "cd ~/www/projects/pxr/; bzr up"
+bzr push --no-strict bzr+ssh://74.207.224.197:2222/projects/pxr/
+ssh  74.207.224.197 -l daquino -p 2222 <<< "cd ~/www/projects/pxr/; bzr up"
