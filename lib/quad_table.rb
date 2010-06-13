a10cbf9d3adbdb1c11d495922c23d7acb062a623
Daniel Aquino <mr.danielaquino@gmail.com>
2010-06-13 01:00:54 -0400

    updated quad to a new name cause this file really created a huge table of quads

--- lib/quad_table.rb
+++ lib/quad_table.rb
@@ -1,5 +1,5 @@
 require "model"
-class Quad < Model
+class QuadTable < Model
 	include Mesh
 	attr_accessor :pos, :orientation, :normal
 	def initialize s
@@ -11,12 +11,12 @@ class Quad < Model
 		@orientation = @normal.quat.normalize
 		@verts = []
 		@primitives = []
-		@scale = s[:scale] || 10
+		@scale = s[:scale] || Vector.new(0,0,0)
 		rows=10
 		cols=10
 		cols.times do |x|
 			rows.times do |y|
-				add x*@scale, y*@scale
+				add x, y
 			end
 		end
 	end
@@ -25,14 +25,14 @@ class Quad < Model
 		@primitives << { 
 			:verts => [ l+0,l+1,l+2,l+3 ],
 #			:verts => [ l+0,l+1,l+2 ],
-#			:pos => @pos.to_a, # any position on plane
-#			:normal => @normal.to_a,
+			:pos => @pos.to_a, # any position on plane
+			:normal => @normal.to_a,
 			:texture => @texture,
 		}
-		@verts << { :vector => [x,y,0], :tu => 0, :tv => 0 }
-		@verts << { :vector => [x,y,0], :tu => 1, :tv => 0 }
-		@verts << { :vector => [x,y,0], :tu => 1, :tv => 1 }
-		@verts << { :vector => [x,y,0], :tu => 0, :tv => 1 }
+		@verts << { :vector => [ x,          y,          0 ], :tu => 0, :tv => 0 }
+		@verts << { :vector => [ x*@scale.x, y,          0 ], :tu => 1, :tv => 0 }
+		@verts << { :vector => [ x*@scale.x, y*@scale.y, 0 ], :tu => 1, :tv => 1 }
+		@verts << { :vector => [ x,          y*@scale.y, 0 ], :tu => 0, :tv => 1 }
 	end
 	def make_dl
 	end
