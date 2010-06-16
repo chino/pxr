f17e654f2e89318603230b960065875c46738646
Daniel Aquino <mr.danielaquino@gmail.com>
2010-06-16 16:40:48 -0400

    added ability to pass a radius for collision detection against bsp tree

--- lib/fskn_bsp.rb
+++ lib/fskn_bsp.rb
@@ -68,31 +68,31 @@ class FsknBsp
 		Plane.new({
 			:pos => node.pos,
 			:normal => node.normal,
-			:scale => Vector.new(2500,2500,2500),
+			:scale => Vector.new(5000,5000,5000),
 			:color => node.color
 		})
 	end
-	def point_inside_groups? pos
+	def point_inside_groups? pos, radius
 		@groups.each_with_index do |group,i|
-			rv,node = point_inside_group?( pos, i )
+			rv,node = point_inside_group?( pos, i, radius )
 			return [rv,node,i] if rv
 		end
 		return [false,nil,-1]
 	end
-	def point_inside_group? pos, group
-		point_inside_tree?( pos, @groups[group][0] )
+	def point_inside_group? pos, group, radius
+		point_inside_tree?( pos, @groups[group][0], radius )
 	end
-	@@epsilon = 0.03125 # wide plane to compensate for math errors
-	def point_inside_tree? pos, node
+	@@epsilon = 0.13125 # wide plane to compensate for math errors
+	def point_inside_tree? pos, node, radius
 		while node
 			d = node.normal.dot( pos ) + node.distance
-			if    d >  @@epsilon
+			if    d >  @@epsilon + radius
 				unless node.front
 					return [true,node]
 				else
 					node = node.front
 				end
-			elsif d < -@@epsilon
+			elsif d < -@@epsilon + radius
 				unless node.back
 					return [false,node]
 				else
@@ -100,10 +100,10 @@ class FsknBsp
 				end
 			else # coincide
 				return [true,node] unless node.front
-				rv,n = point_inside_tree?( pos, node.front )
+				rv,n = point_inside_tree?( pos, node.front, radius )
 				return [true,n] if rv
 				return [false,node] unless node.back
-				rv,n = point_inside_tree?( pos, node.back )
+				rv,n = point_inside_tree?( pos, node.back, radius )
 				return [false,n] unless rv
 				return [true,node]
 			end
