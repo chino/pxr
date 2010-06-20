e8ce28e64f3111cd2decc5f19893b7d1411e6f37
Daniel Aquino <mr.danielaquino@gmail.com>
2010-06-19 22:04:49 -0400

    added some more features to point

--- lib/point.rb
+++ lib/point.rb
@@ -5,18 +5,20 @@ class Point < Model
 	def initialize s
 		@scale = Vector.new 1,1,1
 		@body = nil
-		@pos = Vector.new
+		@pos = s[:pos] || Vector.new
 		@orientation = Quat.new
 		@render_type = GL::POINTS
 		@size = s[:size] || 1.0
 		@verts = []
 		@primitives = []
-		s[:points].each do |pos,color,texture|
+		s[:points].each do |pos,color,texture,transparencies|
 			color ||= [255,255,255,255]
 			@verts << { :vector => pos, :rgba => color }
-			@primitives << { :texture => texture, :verts => [
-				@verts.length - 1
-			]}
+			@primitives << {
+				:texture => texture,
+				:transparencies => transparencies,
+				:verts => [ @verts.length - 1 ]	
+			}
 		end
 	end
 end
